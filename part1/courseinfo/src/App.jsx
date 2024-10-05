a3d6cb150eaab384925e4d54743b5c7ba032675f
tan1193 <hoangtan1193@gmail.com>
2024-10-05 16:40:02 +0700

    fix part 1 not show exercises

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,28 +1,28 @@
-const Header = (course) => {
+const Header = ({course}) => {
   return (
     <h1>{course}</h1>
   )
 }
 
 
-const Content = (part1, part2, part3) => {
+const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
   return (
     <div>
-      <Part part={part1} />
-      <Part part={part2} />
-      <Part part={part3} />
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2}/>
+      <Part part={part3} exercises={exercises3}/>
     </div>
   )
 }
-const Part = (part) => {
+const Part = ({part, exercises}) => {
   return (
     <p>
-      {part.part} 
+      {part} {exercises} 
     </p>
   )
 }
 
-const Total = (exercises1, exercises2, exercises3) => {
+const Total = ({exercises1, exercises2, exercises3}) => {
   return (
     <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
   )
@@ -39,7 +39,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
